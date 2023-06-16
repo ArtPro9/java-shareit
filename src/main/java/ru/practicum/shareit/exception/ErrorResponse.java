e44351f9b749d3709ddc16397f3df14513d92369
ArtPro9 <artem.propazhin@gmail.com>
2023-06-17 00:46:09 +0300

    feat: used lombok

--- src/main/java/ru/practicum/shareit/exception/ErrorResponse.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorResponse.java
@@ -1,13 +1,12 @@
 package ru.practicum.shareit.exception;
 
-public class ErrorResponse {
+import lombok.Getter;
+
+@Getter
+class ErrorResponse {
     private final String error;
 
     public ErrorResponse(String error) {
         this.error = error;
-    }
-
-    public String getError() {
-        return error;
     }
 }
